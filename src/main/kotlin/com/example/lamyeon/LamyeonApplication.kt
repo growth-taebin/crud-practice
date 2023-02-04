c7084c62fdd382959113ac46229c223cad9d3201
yoontaebin <gusckd000@gmail.com>
2023-02-04 16:27:53 +0900

    project setting

---
+++ src/main/kotlin/com/example/lamyeon/LamyeonApplication.kt
@@ -0,0 +1,12 @@
+package com.example.lamyeon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LamyeonApplication
+
+fun main(args: Array<String>) {
+	runApplication<LamyeonApplication>(*args)
+	
+}
---
+++ src/test/kotlin/com/example/lamyeon/LamyeonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lamyeon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LamyeonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
