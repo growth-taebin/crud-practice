e0b822679544c87103cc525d3f5a6f974e0e7bec
yoontaebin <gusckd000@gmail.com>
2023-02-07 22:26:32 +0900

    Add Board Entity

---
+++ src/main/kotlin/com/example/lamyeon/entity/Board.kt
@@ -0,0 +1,25 @@
+package com.example.lamyeon.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Board(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long? = null,
+        var writer: String,
+        var password: String,
+        var title: String,
+        var content: String
+) {
+    fun updatePost(boardFormDto: BoardFormDto) {
+        writer = boardFormDto.writer
+        title = boardFormDto.title
+        password = boardFormDto.password
+        content = boardFormDto.content
+    }
+
+}
