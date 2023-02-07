f30f06d13334e764b0d3562c5e9b9291a43c0a1f
yoontaebin <gusckd000@gmail.com>
2023-02-07 22:30:47 +0900

    Add BoardRepository

---
+++ src/main/kotlin/com/example/lamyeon/repository/BoardRepository.kt
@@ -0,0 +1,9 @@
+package com.example.lamyeon.repository
+
+import com.example.lamyeon.entity.Board
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BoardRepository: JpaRepository<Board, Long> {
+}
