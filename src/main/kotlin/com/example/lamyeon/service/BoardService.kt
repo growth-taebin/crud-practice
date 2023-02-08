1997d9b4327abe29ffbd02fa95bca7b64bd7922e
yoontaebin <gusckd000@gmail.com>
2023-02-08 18:23:49 +0900

    Update service 반환 값 수정

--- src/main/kotlin/com/example/lamyeon/service/BoardService.kt
+++ src/main/kotlin/com/example/lamyeon/service/BoardService.kt
@@ -1,35 +1,48 @@
 package com.example.lamyeon.service
 
 import com.example.lamyeon.dto.BoardFormDto
+import com.example.lamyeon.dto.ResponseDto
 import com.example.lamyeon.entity.Board
 import com.example.lamyeon.repository.BoardRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
-class BoardService @Autowired constructor(
+class BoardService(
         val boardRepository: BoardRepository,
 ) {
 
-    fun write(boardFormDto: BoardFormDto): Long? {
-        return boardRepository.save(Board(id = -1 ,boardFormDto.writer, boardFormDto.password, boardFormDto.title, boardFormDto.content)).id
+    fun write(
+            boardFormDto: BoardFormDto
+    ): Long? {
+        return boardRepository.save(
+                ResponseDto(id = -1, boardFormDto.writer, boardFormDto.password, boardFormDto.title, boardFormDto.content)
+        ).id
     }
 
-    fun getPost(id: Long): Board? {
-        return boardRepository.findById(id).get()
-    }
+    fun getPost(
+            id: Long
+    ): ResponseDto =
+            boardRepository.findById(id).get()
+                    .let { return it }
 
-    fun deletePost(id: Long) {
+    fun deletePost(
+            id: Long
+    ) {
         boardRepository.deleteById(id)
     }
 
-    fun updatePost(id: Long, boardFormDto: BoardFormDto): Board {
-        val post = boardRepository.findById(id).get()
-        post.updatePost(boardFormDto)
-        return post
-    }
+    fun updatePost(
+            id: Long,
+            boardFormDto: BoardFormDto
+    ): ResponseDto =
+            boardRepository.findById(id).get()
+                    .let { updatePost(id, boardFormDto) }
+                    .let { return it  }
 
-    fun getPostList(): List<Board> {
+    fun getPostList(): List<ResponseDto> {
         return boardRepository.findAll()
     }
 }
