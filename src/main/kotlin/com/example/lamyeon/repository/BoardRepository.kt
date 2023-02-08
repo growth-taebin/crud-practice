d4a0bf926d835c541f5ae892e11045829fb6ed29
yoontaebin <gusckd000@gmail.com>
2023-02-08 18:22:30 +0900

    Update repository 반환 값 수정

--- src/main/kotlin/com/example/lamyeon/repository/BoardRepository.kt
+++ src/main/kotlin/com/example/lamyeon/repository/BoardRepository.kt
@@ -1,9 +1,10 @@
 package com.example.lamyeon.repository
 
+import com.example.lamyeon.dto.ResponseDto
 import com.example.lamyeon.entity.Board
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BoardRepository: JpaRepository<Board, Long> {
+interface BoardRepository: JpaRepository<ResponseDto, Long> {
 }
